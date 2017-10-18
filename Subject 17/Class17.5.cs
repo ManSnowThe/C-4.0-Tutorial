c667c9bec87c71b3762a43f7ab8e8eedf807beec
Alexander <32848544+ManSnowThe@users.noreply.github.com>
2017-10-19 00:52:25 +0600

    Add files via upload

---
+++ Subject 17/Class17.5.cs
@@ -0,0 +1,64 @@
+﻿// Анализ методов с помощью рефлексии.using System;
+using System.Reflection;
+
+namespace ca2
+{
+    class MyClass
+    {
+        int x;
+        int y;
+        public MyClass(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+        public int Sum()
+        {
+            return x + y;
+        }
+        public bool IsBetween(int i)
+        {
+            if (x < i && i < y) return true;
+            else return false;
+        }
+        public void Set(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+        public void Show()
+        {
+            Console.WriteLine(" x: {0}, y: {1}", x, y);
+        }
+    }
+    class ReflectDemo
+    {
+        static void Main()
+        {
+            Type t = typeof(MyClass); // Получить объект класса Type, представляющий класс MyClass
+
+            Console.WriteLine("Анализ методов, определенных в классе " + t.Name);
+            Console.WriteLine();
+            Console.WriteLine("Поддерживаемые методы: ");
+            MethodInfo[] mi = t.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+
+            // Вывести методы, поддерживаемые в классе MyClass.
+            foreach(MethodInfo m in mi)
+            {
+                // Вывести возвращаемый тип и имя каждого метода.
+                Console.Write(" " + m.ReturnType.Name + " " + m.Name + "(");
+
+                // Вывести параметры.
+                ParameterInfo[] pi = m.GetParameters();
+                for(int i = 0; i < pi.Length; i++)
+                {
+                    Console.Write(pi[i].ParameterType.Name + " " + pi[i].Name);
+                    if (i + 1 < pi.Length) Console.Write(", ");
+                }
+                Console.WriteLine(")");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Subject 17/Class17.8(1).cs
@@ -0,0 +1,72 @@
+﻿// Файл, содержащий три класса и носящий имя MyClasses.cs.
+using System;
+
+namespace ca2
+{
+    class MyClass
+    {
+        int x;
+        int y;
+
+        public MyClass(int i)
+        {
+            Console.WriteLine("Конструирование класса MyClass(int). ");
+            x = y = i;
+            Show();
+        }
+        public MyClass(int i, int j)
+        {
+            Console.WriteLine("Конструирование класса MyClass(int, int). ");
+            x = i;
+            y = j;
+            Show();
+        }
+        public int Sum()
+        {
+            return x + y;
+        }
+        public bool IsBetween(int i)
+        {
+            if ((x < i) && (i < y)) return true;
+            else return false;
+        }
+        public void Set(int a, int b)
+        {
+            Console.Write("В методе Set(int, int). ");
+            x = a;
+            y = b;
+            Show();
+        }
+        // Перегрузить метод Set.
+        public void Set(double a, double b)
+        {
+            Console.Write("В методе Set(double, double). ");
+            x = (int)a;
+            y = (int)b;
+            Show();
+        }
+        public void Show()
+        {
+            Console.WriteLine("Значение x: {0}, значение у: {1}", x, y);
+        }
+    }
+    class AnotherClass
+    {
+        string msg;
+        public AnotherClass(string str)
+        {
+            msg = str;
+        }
+        public void Show()
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Demo
+    {
+        static void Main()
+        {
+            Console.WriteLine("Это заполнитель.");
+        }
+    }
+}
